0caa695df8ba04903711d5d754f4263e945fca0d
Will <will@thefixitgeek.com>
2015-09-10 22:51:43 -0600

    testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,18 @@ after_validation :geocode
 
 
 
+# geocoded_by :postal_code,
+#   :latitude => :lat, :longitude => :lon
+# after_validation :geocode
+
+# http://stackoverflow.com/questions/6770784/how-to-create-popup-window-to-create-new-record-in-rails-3
+
+
   def user_ip
   	request.location
   end
 
 end
 
+ 	
 
