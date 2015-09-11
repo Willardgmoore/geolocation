d72274f434c4500968d3bd3042ea171b367e0ee7
Will <will@thefixitgeek.com>
2015-09-10 21:19:09 -0600

    testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ActiveRecord::Base
   # include Geocoder::Model::Mongoid
-  geocoded_by :user_ip
+geocoded_by :ip_address,
+  :latitude => :lat, :longitude => :lon
+after_validation :geocode
 
-  after_validation :geocode
 
 
   def user_ip
-  	# request.location
+  	request.location
   end
 
 end
+
+
